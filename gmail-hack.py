de48f4ec15489b77670787ebd6c68d892c779e11
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-24 11:46:55 +0100

    Add files via upload

---
+++ gmail-hack.py
@@ -0,0 +1,12 @@
+import smtplib
+import os
+import sys
+import requests
+red = '\033[31m'
+yellow = '\033[93m'
+green = '\033[92m'
+clear = '\033[0m'
+bold = '\033[01m'
+cyan = '\033[96m'
+cy='\033[095m'
+cya='\033[035m'
